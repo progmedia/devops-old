ccbae294d44dc4856cf1c6107c6e6b894a56cd70
Adam McCann <adam.mccann@videogamer.com>
2013-10-23 10:13:24 +0100

    Added parselog script & git update script to new devops folder...

---
+++ update.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# updateRepos
+#
+# Updated all git repositories in the first argument (or if not specified, the current directory), excluding any directories named 'vendor'.
+#
+
+DIR=$(pwd)
+
+# if directory provided, make it absolute
+if [ ! -z $1 ]
+then
+    DIR=$(readlink -f $1)
+fi
+
+# Sanity check
+if [ ! -d $DIR ]; then
+    echo "Error: Directory '$DIR' does not exist!"
+    exit 1
+fi
+
+# Find all files containing the directory .git
+FILES=$(find $DIR -type d -name .git)
+
+echo -e "\033[32mPulling all git directories in $DIR\033[0m"
+
+# Iterate directories, excluding vendor
+for d in $FILES; do
+    if [[ "$d" != *\/vendor\/* ]]
+    then
+	DIR=${d%/*}
+	echo -e "\033[36mPulling $DIR\033[0m"
+	cd $DIR
+	git pull
+    fi
+done
+
+echo -e "\033[32mDone.\033[0m"
+
+exit 0
