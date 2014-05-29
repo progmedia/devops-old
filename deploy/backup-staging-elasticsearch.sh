5b00a95b9d57b4751757b30ea4e632b55d129c26
Alex Bilbie <hello@alexbilbie.com>
2014-05-29 17:18:59 +0100

    Removed old files

--- deploy/backup-staging-elasticsearch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ansible-playbook -i hosts.ini playbooks/backup-staging-elasticsearch.yml -u ansible --private-key=~/.ssh/id_rsa -v
--- deploy/download-migrations-data-locally.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ansible-playbook -i hosts.ini playbooks/download-migrations-data-locally.yml -u ansible --private-key=~/.ssh/id_rsa -v
